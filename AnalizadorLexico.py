1dbf3e440ad21c36a77a5c469aecc0862942340b
Sergio Perez <43412800+sergioi@users.noreply.github.com>
2019-12-05 17:12:48 -0600

    Add files via upload

---
+++ AnalizadorLexico.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# desarollo de aanalizardor lexico
+#liga https://github.com/GeovaniTuz/PythonU4and5
+
+import ply.lex as lex  # inportacion de librerias necesarias
+import re
+
+resultado_lexema = []
+
+
+tokens = [
+    # inicio y final
+    'TAGINICIO', 'TAG_FINAL',
+
+    'IDENTIFICADOR', 'ENTERO', 'ASIGNAR', 'SUMA', 'RESTA', 'MULT', 'DIV', 'POTENCIA', 'MODULO',
+    'MINUSMINUS', 'PLUSPLUS','PUNTOYCOMA','PUNTO','COMA','DECIMAL','VARIABLE','COMENTARIO',
+    # Condiones
+    'SI', 'SINO', 'SIErr',
+    # Ciclos
+    'MIENTRAS', 'PARA',
+    # logica
+    'AND', 'OR', 'NOT', 'MENORQUE', 'MENORIGUAL', 'MAYORQUE', 'MAYORIGUAL', 'IGUAL', 'DISTINTO',
+    # Symbolos
+    'NUMERAL', 'PARIZQ', 'PARDER', 'CORIZQ', 'CORDER', 'LLAIZQ', 'LLADER'
+]
+
+# Reglas de Expresiones Regualres para token de Contexto simple
+#t_PUNTO = r'[+,-]?[[0-9]*[.]]?[0-9]+'
+#[+,-]?[[0-9]*[.]]?[0-9]+t__COMA = r'\,'
+t_PUNTOYCOMA = r';'
+t_SUMA = r'\+'
+t_RESTA = r'-'
+t_MINUSMINUS = r'\-\-'
+#t_PUNTO = r'\.'
+t_MULT = r'\*'
+t_DIV = r'/'
+t_MODULO = r'\%'
+t_POTENCIA = r'(\*{2} | \^)'
+t_ASIGNAR = r'='
+# Expresiones
+t_AND = r'\&\&'
+t_OR = r'\|{2}'
+t_NOT = r'\!'
+t_MENORQUE = r'<'
+t_MAYORQUE = r'>'
+t_PARIZQ = r'\('
+t_PARDER = r'\)'
+t_CORIZQ = r'\['
+t_CORDER = r'\]'
+t_LLAIZQ = r'{'
+t_LLADER = r'}'
+#recorte
+
+#recorte
+
+text = ""
+for linea in archivo:
+    text += linea
+prueba(text)
+print('\n'.join(list(map(''.join, resultado_lexema)))) #AL IMPRIMIR LOS DATOS, ESTO LO ORDENA DE MANERA ESTRUCTURADA
